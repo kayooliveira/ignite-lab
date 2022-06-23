d6294df56bff52803e6365f082d1cf0a9281332d
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-23 16:41:23 -0300

    feat(Now the logo on header is a link to home): :sparkles:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 import { Logo } from '../Logo'
 
 export function Header() {
   return (
-    <header className="flex w-full items-center justify-center border-b border-ignite-gray-7 bg-ignite-gray-2 py-5">
-      <Logo />
+    <header className="z-10 flex w-full items-center justify-center border-b border-ignite-gray-7 bg-ignite-gray-2 py-5">
+      <Link to="/">
+        <Logo />
+      </Link>
     </header>
   )
 }
