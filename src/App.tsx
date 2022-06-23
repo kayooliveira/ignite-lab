fbd0988eccd503c85b74ecc4f8744e8414311d90
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-23 16:41:36 -0300

    feat(Adding Toaster provider to app): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client'
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 import { client } from './lib/apollo'
@@ -9,6 +10,7 @@ export function App() {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
+        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
         <Router />
       </BrowserRouter>
     </ApolloProvider>
