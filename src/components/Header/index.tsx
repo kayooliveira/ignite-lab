5c79eed7f0a98af013de1a5dbadb17a39e441427
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-23 00:45:18 -0300

    feat(Creating header component): :sparkles:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
 
+import { Logo } from '../Logo'
+
 export function Header() {
-  return <header></header>
+  return (
+    <header className="flex w-full items-center justify-center border-b border-ignite-gray-7 bg-ignite-gray-2 py-5">
+      <Logo />
+    </header>
+  )
 }
