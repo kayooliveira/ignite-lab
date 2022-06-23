503d71ae63b1036a9e0a513c19b436d7cb431385
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-23 16:41:07 -0300

    feat(Removing validation before show player): :sparkles:

--- src/pages/Event/index.tsx
+++ src/pages/Event/index.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react'
-import { useParams } from 'react-router-dom'
 
 import { Header } from '../../components/Header'
 import { Player } from '../../components/Player'
 import { Sidebar } from '../../components/Sidebar'
 
 export function Event() {
-  const { slug } = useParams<{ slug: string }>()
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex flex-1">
-        {slug ? <Player /> : <div className="flex-1" />}
+        <Player />
         <Sidebar />
       </main>
     </div>
