db17f96dfae7bdf4cf9792c85c17b7d5d4f2e5af
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-23 16:41:49 -0300

    feat(Creating loading page): :sparkles:

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Logo } from '../Logo'
+
+export function Loading() {
+  return (
+    <div className=" flex max-w-full flex-1 flex-col items-center justify-center bg-ignite-gray-1">
+      <div className="flex animate-bounce leading-relaxed">
+        <Logo />
+      </div>
+
+      <span className="mt-2 block text-lg font-bold text-ignite-green">
+        Clique em uma aula para começar!
+      </span>
+    </div>
+  )
+}
