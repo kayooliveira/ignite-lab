d77d62b91e9d3a977c0d879d19f19b515f17729c
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-23 16:40:01 -0300

    feat(Leaving the home with a more pleasant look): :sparkles:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import toast from 'react-hot-toast'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { Logo } from '../../components/Logo'
 
 export function Home() {
+  const navigate = useNavigate()
+  function redirectToEventPage() {
+    navigate('/evento')
+  }
+
+  useEffect(() => {
+    toast('Aguarde! Você será redirecionado para o evento em 3 segundos')
+    setTimeout(() => {
+      redirectToEventPage()
+    }, 3000)
+  }, [])
+
   return (
     <div className="flex h-screen w-screen items-center justify-center text-center">
-      <div>
-        <h1 className="text-2xl font-bold leading-relaxed">
-          Bem vindo ao Ignite Labs
-        </h1>
-        Clique{' '}
-        <Link className="text-ignite-green underline" to="/evento">
-          aqui
-        </Link>{' '}
-        para acessar a página do evento.
+      <div className="flex flex-col items-center justify-center">
+        <Link to="/evento" className="text-2xl font-bold leading-relaxed">
+          <Logo />
+        </Link>
       </div>
     </div>
   )
