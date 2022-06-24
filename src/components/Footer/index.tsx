c640bd5d44c4f971718a470b2a74ba79c15d7600
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-24 00:01:58 -0300

    fix(Updating footer background color): :art:

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { RocketseatLogo } from '../RocketseatLogo'
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between border-t border-ignite-gray-7 px-8 pt-6 pb-4">
+    <footer className="flex items-center justify-between border-t border-ignite-gray-7 bg-ignite-gray-1 px-8 py-4">
       <div className="flex items-center">
         <RocketseatLogo />
         <span className="ml-6 leading-relaxed text-ignite-gray-6">
