ad0ca46620eb24b1595ea0f28d218d2278855baf
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-24 23:18:12 -0300

    feat(Starting using graphql-code-generator and implementing it on code): :sparkles: overrinding all old queries and mutations for using the hooks generated by graphql-code-generator

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,54 +1,20 @@
-import { gql, useQuery } from '@apollo/client'
 import { DefaultUi, Player as VimePlayer, Youtube } from '@vime/react'
+import { isPast } from 'date-fns'
 import { DiscordLogo, FileArrowDown, Image, Lightning } from 'phosphor-react'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import { useGetLessonBySlugQuery } from '../../graphql/graphqlCodegen'
 import { Card } from '../Card'
 import { Footer } from '../Footer'
-
-import '@vime/core/themes/default.css'
 import { NoData } from '../NoData'
 import { PlayerSkelleton } from '../PlayerSkelleton'
 
-import { isPast } from 'date-fns'
-
-const GET_LESSON_BY_SLUG = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      id
-      slug
-      title
-      videoId
-      description
-      availableAt
-      teacher {
-        avatarUrl
-        bio
-        name
-      }
-    }
-  }
-`
-interface Lesson {
-  title: string
-  videoId: string
-  description: string
-  availableAt: string
-  teacher: {
-    name: string
-    bio: string
-    avatarUrl: string
-  }
-}
-
-interface GetLessonBySlug {
-  lesson: Lesson
-}
+import '@vime/core/themes/default.css'
 
 export function Player() {
   const { slug } = useParams<{ slug: string }>()
-  const { data } = useQuery<GetLessonBySlug>(GET_LESSON_BY_SLUG, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug
     },
@@ -68,7 +34,7 @@ export function Player() {
   }
 
   return (
-    <div className="mt-[1px] flex-1">
+    <div className="mt-[1px] max-h-full flex-1 overflow-y-scroll scrollbar-thin scrollbar-thumb-ignite-primary-dark  scrollbar-track-ignite-gray-2">
       <div className="flex justify-center bg-black">
         <div className="aspect-video h-full w-full max-w-[1100px] rounded bg-ignite-gray-3">
           <VimePlayer>
@@ -77,40 +43,43 @@ export function Player() {
           </VimePlayer>
         </div>
       </div>
-      <div className="flex items-start gap-[3.75rem] px-8 pt-8 pb-6">
+      <div className="flex flex-col items-start gap-[3.75rem] px-8 pt-8 lg:flex-row">
         <div className="flex-1">
           <h1 className="mb-4 text-2xl font-bold leading-snug">
             {data.lesson?.title}
           </h1>
           <p className="leading-relaxed">{data.lesson?.description}</p>
+
+          {data.lesson.teacher && (
+            <div className="flex items-center gap-4 py-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-ignite-secondary"
+                src={data.lesson?.teacher.avatarUrl}
+                alt="Avatar do professor"
+              />
+              <div className="leading-relaxed">
+                <strong className="block text-2xl font-bold leading-snug">
+                  {data.lesson?.teacher.name}
+                </strong>
+                <span className="font block text-sm text-ignite-gray-6">
+                  {data.lesson?.teacher.bio}
+                </span>
+              </div>
+            </div>
+          )}
         </div>
-        <div className="flex flex-col gap-4">
-          <button className="flex items-center rounded bg-ignite-primary px-6 py-4 font-bold leading-relaxed transition-colors hover:bg-ignite-primary-dark">
+        <div className="flex w-full flex-col gap-4 lg:w-auto">
+          <button className="flex items-center justify-center rounded bg-ignite-primary px-6 py-4 font-bold leading-relaxed transition-colors hover:bg-ignite-primary-dark">
             <DiscordLogo width="24" height="24" />
             COMUNIDADE NO DISCORD
           </button>
-          <button className="flex items-center rounded border border-ignite-secondary bg-transparent px-6 py-4 font-bold leading-relaxed text-ignite-secondary transition-colors hover:bg-ignite-secondary hover:text-ignite-gray-1">
+          <button className="flex items-center justify-center rounded  border border-ignite-secondary bg-transparent px-6 py-4 font-bold leading-relaxed text-ignite-secondary transition-colors hover:bg-ignite-secondary hover:text-ignite-gray-1">
             <Lightning width="24" height="24" />
             ACESSE O DESAFIO
           </button>
         </div>
       </div>
-      <div className="flex items-center gap-4 px-8">
-        <img
-          className="h-16 w-16 rounded-full border-2 border-ignite-secondary"
-          src={data.lesson?.teacher.avatarUrl}
-          alt="Avatar do professor"
-        />
-        <div className="leading-relaxed">
-          <strong className="block text-2xl font-bold leading-snug">
-            {data.lesson?.teacher.name}
-          </strong>
-          <span className="font block text-sm text-ignite-gray-6">
-            {data.lesson?.teacher.bio}
-          </span>
-        </div>
-      </div>
-      <div className="flex items-center justify-center gap-8 px-8 py-20">
+      <div className="flex flex-col items-stretch justify-center gap-8 px-8 py-20 lg:flex-shrink-0 lg:items-center">
         <Card
           title="Material complementar"
           content="Acesse o material complementar para acelerar o seu desenvolvimento"
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,50 +1,50 @@
-import { gql, useQuery } from '@apollo/client'
-import React from 'react'
+import { List } from 'phosphor-react'
+import React, { useState } from 'react'
 
+import { useGetLessonsQuery } from '../../graphql/graphqlCodegen'
 import { Lesson } from '../Lesson'
-
-const GET_LESSONS_QUERY = gql`
-  query GetLessons {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      slug
-      title
-      availableAt
-    }
-  }
-`
-interface Lesson {
-  id: string
-  title: string
-  slug: string
-  availableAt: string
-  lessonType: 'live' | 'class'
-}
-
-interface GetLessonsQueryResponse {
-  lessons: Lesson[]
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
-
+  const { data } = useGetLessonsQuery()
+  const [isOpen, setIsOpen] = useState(true)
+  function toggleSidebar() {
+    setIsOpen(!isOpen)
+  }
   return (
-    <aside className="mt-[1px] flex w-[21.75rem] min-w-[21.75rem] flex-col border-l border-ignite-gray-7 bg-ignite-gray-2 p-6">
-      <span className="mb-6 block border-b border-ignite-gray-7 pb-6 text-2xl font-bold">
-        Cronograma de aulas
-      </span>
-      <div className="flex flex-col gap-8">
-        {data?.lessons.map(lesson => (
-          <Lesson
-            key={lesson.id}
-            availableAt={new Date(lesson.availableAt)}
-            slug={lesson.slug}
-            type={lesson.lessonType}
-            title={lesson.title}
-          />
-        ))}
-      </div>
-    </aside>
+    <>
+      <button
+        onClick={toggleSidebar}
+        className="absolute top-4 right-4 z-50 flex items-start gap-2 text-ignite-primary md:hidden md:items-start"
+      >
+        <strong className="text-ignite-white">Aulas</strong>
+        <List weight="bold" width="24" height="24" />
+      </button>
+      <aside
+        className={`${
+          isOpen
+            ? 'fixed right-0 md:static md:flex'
+            : 'fixed -right-full md:static md:flex'
+        } z-50 mt-[1px] max-h-screen w-[21.75rem]
+      min-w-[21.75rem]
+     animate-slideFromRight flex-col overflow-y-scroll border-l border-ignite-gray-7
+    bg-ignite-gray-2 p-6 transition-all scrollbar-thin
+    scrollbar-thumb-ignite-primary-dark scrollbar-track-ignite-gray-2
+     `}
+      >
+        <span className="mb-6 block border-b border-ignite-gray-7 pb-6 text-2xl font-bold">
+          Cronograma de aulas
+        </span>
+        <div className="flex flex-col gap-8">
+          {data?.lessons.map(lesson => (
+            <Lesson
+              key={lesson.id}
+              availableAt={new Date(lesson.availableAt)}
+              slug={lesson.slug || ''}
+              type={lesson.lessonType}
+              title={lesson.title}
+            />
+          ))}
+        </div>
+      </aside>
+    </>
   )
 }
