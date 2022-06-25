51a75c03ae7f1393442b0d5ee6add0a433ef1774
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-25 00:23:14 -0300

    style(Making footer responsible): :art:

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,8 @@ import { RocketseatLogo } from '../RocketseatLogo'
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between border-t border-ignite-gray-7 bg-ignite-gray-1 px-8 py-4">
-      <div className="flex items-center">
+    <footer className="flex flex-col items-center justify-between border-t border-ignite-gray-7 bg-ignite-gray-1 px-8 py-4 text-xs md:flex-row">
+      <div className="flex flex-col items-center lg:flex-row">
         <RocketseatLogo />
         <span className="ml-6 leading-relaxed text-ignite-gray-6">
           Rockeseat - Todos os direitos reservados
