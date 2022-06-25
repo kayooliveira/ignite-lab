50f5b6aae68af8dc8d5756fa0ba31026c9ca73b3
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-25 00:23:30 -0300

    style(Making header responsible): :art:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,8 @@ import { Logo } from '../Logo'
 
 export function Header() {
   return (
-    <header className="z-10 flex w-full items-center justify-center border-b border-ignite-gray-7 bg-ignite-gray-2 py-5">
-      <Link to="/">
+    <header className="r z-10 flex w-full items-center justify-start border-b border-ignite-gray-7 bg-ignite-gray-2 py-5 md:justify-center">
+      <Link to="/" className="ml-8 md:ml-0">
         <Logo />
       </Link>
     </header>
