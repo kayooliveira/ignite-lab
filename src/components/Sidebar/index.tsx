6a362c18ee14e472fe7b38628f92c42336d2aaf3
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-25 00:33:48 -0300

    feat(When the user select a lesson, the sidebar closes): :sparkles:

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,19 @@
 import { List } from 'phosphor-react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import { useGetLessonsQuery } from '../../graphql/graphqlCodegen'
 import { Lesson } from '../Lesson'
 export function Sidebar() {
+  const { slug } = useParams<{ slug: string }>()
   const { data } = useGetLessonsQuery()
   const [isOpen, setIsOpen] = useState(true)
   function toggleSidebar() {
     setIsOpen(!isOpen)
   }
+  useEffect(() => {
+    setIsOpen(false)
+  }, [slug])
 
   return (
     <>
