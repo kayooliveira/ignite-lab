17890726a6330dbcab8823417b46b8ce7975e2a6
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-25 16:23:16 -0300

    feat(Changing sidebar action collors and fixing the width on small screens): :sparkles:

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -21,7 +21,7 @@ export function Sidebar() {
     <>
       <button
         onClick={toggleSidebar}
-        className="gap-2text-ignite-primary absolute top-6 right-8 z-[49] flex items-start md:hidden md:items-start"
+        className="absolute top-6 right-8 z-[49] flex items-start gap-2 text-ignite-primary md:hidden md:items-start"
       >
         <strong className="text-ignite-white">Aulas</strong>
         <List weight="bold" width="24" height="24" />
@@ -31,16 +31,19 @@ export function Sidebar() {
           isOpen
             ? 'fixed right-0 top-0 md:static md:flex'
             : 'fixed -right-full top-0 md:static md:flex'
-        } z-50 mt-[1px] max-h-full w-[21.75rem] min-w-[21.75rem] animate-slideFromRight
-      flex-col
-      overflow-y-scroll border-l border-ignite-gray-7
-     bg-ignite-gray-2 p-6
-    transition-all scrollbar-thin scrollbar-thumb-ignite-primary-dark scrollbar-track-ignite-gray-1
+        } z-50 mt-[1px]  max-h-full  w-screen min-w-[21.75rem] animate-slideFromRight flex-col
+      overflow-y-scroll
+      border-l border-ignite-gray-7 bg-ignite-gray-2
+     p-6 transition-all
+    scrollbar-thin scrollbar-thumb-ignite-primary-dark scrollbar-track-ignite-gray-1 lg:w-[21.75rem]
      `}
       >
         <div className="mb-6 flex items-center justify-between border-b border-ignite-gray-7 pb-6 lg:justify-start ">
           <span className=" block text-2xl font-bold">Cronograma de aulas</span>
-          <button className="lg:hidden" onClick={toggleSidebar}>
+          <button
+            className="text-ignite-primary lg:hidden"
+            onClick={toggleSidebar}
+          >
             <X weight="bold" width="24" height="24" />
           </button>
         </div>
